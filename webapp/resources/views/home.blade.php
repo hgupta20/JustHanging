cf84ca3d60d8f6fa9b1c8451a23defcf02bc266d
hgupta20 <harshdevprakashgupta@outlook.com>
2019-07-20 20:01:24 -0500

    Added posts and Database

--- webapp/resources/views/home.blade.php
+++ webapp/resources/views/home.blade.php
@@ -2,22 +2,62 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class = "row">
+        <div class = "col-3 p-5">
+            <img src = "/images/profile.jpg" class="rounded-circle" style="width:180px;height:190px"> 
+        </div>
+        <div class = "col-9 pt-5">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+            <div><h1>abstract_art</h1></div>
+            <div class="d-flex">
+                <div class = "pr-5"><strong>180</strong> posts </div>
+                <div class = "pr-5" ><strong>23k</strong> followers </div>
+                <div class = "pr-5"><strong>233</strong> following </div>
             </div>
+            <div class = "pt-4 font-weight-bold">abstractart.org</div>
+            <div>We are a group of designers sharing abstract art.</div>
+            <div><a href="#">www.abstractart.com</a></div>
+                
         </div>
     </div>
+    <div class="row pl-5 pt-5 pr-5">
+        <div class = "col-4">
+            <img src = "/images/1.jpeg" width="500" height="300" class = "w-100">
+        </div>
+        <div class = "col-4">
+            <img src = "/images/2.jpeg" width="500" height="300" class = "w-100">
+        </div>
+        <div class = "col-4">
+            <img src = "/images/3.jpeg"  width="500" height="300" class = "w-100">
+        </div>
+        <div class = "col-4 pt-4">
+            <img src = "/images/4.jpeg"  width="500" height="300" class = "w-100">
+        </div>
+        <div class = "col-4 pt-4">
+            <img src = "/images/5.jpeg" width="500" height="300" class = "w-100">
+        </div>
+        <div class = "col-4 pt-4">
+            <img src = "/images/6.jpeg" width="500" height="300" class = "w-100">
+        </div>
+        <div class = "col-4 pt-4">
+            <img src = "/images/7.jpeg" width="500" height="300" class = "w-100">
+        </div>
+        <div class = "col-4 pt-4">
+            <img src = "/images/8.jpeg" width="500" height="300" class = "w-100">
+        </div>
+        <div class = "col-4 pt-4">
+            <img src = "/images/9.jpeg" width="500" height="300" class = "w-100">
+        </div>
+        <div class = "col-4 pt-4">
+            <img src = "/images/10.jpeg" width="500" height="300" class = "w-100">
+        </div>
+        <div class = "col-4 pt-4">
+            <img src = "/images/11.jpeg" width="500" height="300" class = "w-100">
+        </div>
+        <div class = "col-4 pt-4">
+            <img src = "/images/12.jpeg" width="500" height="300" class = "w-100">
+        </div>
+    </div>
+    
 </div>
 @endsection
